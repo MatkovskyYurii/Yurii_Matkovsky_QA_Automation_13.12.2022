20416333ba80b189343e1aa3cb0e7de886689361
Yurii Matkovsky <matkovskyyurii@gmail.com>
2022-12-27 11:20:13 +0200

    hello git push

---
+++ src/main/java/com/hiilel/lessons/lesson3/HelloGit.java
@@ -0,0 +1,10 @@
+package com.hiilel.lessons.lesson3;
+
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+        System.out.println("Hello git");
+    }
+}
