1a83f87533f455257e9f27634807cf239495806c
Yurii Matkovsky <matkovskyyurii@gmail.com>
2023-01-11 15:09:15 +0200

    Homework 9 upd added Task 9 description

--- src/main/java/com/hiilel/lessons/lesson7/homework9/Main.java
+++ src/main/java/com/hiilel/lessons/lesson7/homework9/Main.java
@@ -3,7 +3,13 @@ package com.hiilel.lessons.lesson7.homework9;
 import java.util.Arrays;
 
 public class Main {
-
+//    Є дві команди регбі з 25 гравців різного віку у кожній.
+//
+//    Вік беремо випадковим чином в діапазоні від 18 до 40.
+//
+//    Виведіть у двох рядках вік гравців кожної команди.
+//
+//    Порахуйте середній вік гравців кожної команди та виведіть на екран.
     public static void main(String[] args) {
 
         int[] team1 = new int[25];
